771bce1c1d31bae7d9722fd3575dc0a2ca974d49
Yi Yan Tay <yiyantay@gmail.com>
2015-07-19 14:22:04 +0800

    readme added

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Memento
 
-a [Sails](http://sailsjs.org) application
+A [Sails](http://sailsjs.org) application. User can upload photo through website (should not ultimately in the product) and through iOS app (another repository). Photos are displayed in realtime (pseudo-realtime actually since I have not actually discovered how to make a page automatically refresh. 
+
+## Team 
+1. Ian Tay (Website, Server), 
+2. Clement Lee (iOS - app), 
+3. Zac Lim (iOS - BLE beacon), 
+4. Gerald Tan (Product Design)
--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -1,21 +1,27 @@
 /**
- * UploadController
- *
- * @description :: Server-side logic for managing Uploads
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
+* UploadController
+*
+* @description :: Server-side logic for managing Uploads
+* @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+*/
 
 module.exports = {
-	
 
 
-  /**
-   * `UploadController.upld()`
-   */
-  upld: function (req, res) {
-    return res.json({
-      todo: 'upld() is not implemented yet!'
-    });
-  }
+
+	/**
+	* `UploadController.upld()`
+	*/
+	upld: function (req, res) {
+		console.log(req.file);
+		req.file('photo').upload({ dirname: '.'},function (err, files) {
+			if (err){
+			return res.serverError(err);
+			}
+			return res.json({
+				message: files.length + ' file(s) uploaded successfully!',
+				files: files
+			});
+		});
+	}
 };
-
