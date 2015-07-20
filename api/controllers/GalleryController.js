fefccbedbe769255633075778e700ed8dc7470e0
Yi Yan Tay <yiyantay@gmail.com>
2015-07-20 21:24:38 +0800

    Added viewalt to GalleryController for iOS compatibility.

--- api/controllers/GalleryController.js
+++ api/controllers/GalleryController.js
@@ -13,6 +13,15 @@ module.exports = {
 			console.log(files);
 			return res.view('gallery', {galleryfiles: files})
 		});
+	},
+
+	viewalt: function (req, res) {
+		var fs = require('fs');
+		fs.readdir(require('path').join(__dirname,'/../../.tmp/public/uploads'), function (err, files) {
+			console.log(require('path').join(__dirname,'/../../.tmp/public/uploads'));
+			console.log(files);
+			res.locals.layout = 'layout2'; //change layout so that it works on iOS
+			return res.view('gallery', {galleryfiles: files})
+		});
 	}
 }
-
