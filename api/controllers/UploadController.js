c1caaaa96a797ded9c28fe07c0efcdcb5e55cb61
Ian <yiyantay@gmail.com>
2015-07-21 05:12:03 -0400

    multiple upload locations added. gallery to be updated.

--- api/controllers/UploadController.js
+++ api/controllers/UploadController.js
@@ -1,31 +1,38 @@
 /**
-* UploadController
-*
-* @description :: Server-side logic for managing Uploads
-* @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
-*/
+ * UploadController
+ *
+ * @description :: Server-side logic for managing Uploads
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
 
 module.exports = {
 
 
 
 	/**
-	* `UploadController.upld()`
-	*/
+	 * `UploadController.upld()`
+	 */
 	upld: function (req, res) {
-		console.log(req.file);
-		req.file('photo').upload({ dirname: '.'},function (err, files) {
-			if (err){
-				return res.serverError(err);
-			}
-			return res.view('homepage',{
+		var params = req.params.all();
+		console.log(params);
+		var this_location = params.location;
+		console.log(this_location);
+		if (typeof this_location == "string"){
+			req.file('photo').upload({ dirname:require('path').join('./',this_location)},function (err, files) {
+				if (err){
+					return res.serverError(err);
+				}
+				return res.view('homepage',{
 
-				// not working, need implementation of flash messages
+					// not working, need implementation of flash messages
 
 					message: files.length + ' file(s) uploaded successfully!',
-					files: files
+				       files: files
 				}
-			);
-		});
+				);
+			});
+		}else{
+			console.log('not string')
+		}
 	}
 };
