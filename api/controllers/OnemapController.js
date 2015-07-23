cf4d8f6e7cabf7a58826ce73dea82b9a1304ead2
Yi Yan Tay <yiyantay@gmail.com>
2015-07-23 14:11:52 +0800

    onemap layout

--- api/controllers/OnemapController.js
+++ api/controllers/OnemapController.js
@@ -1,12 +1,12 @@
 /**
- * OnemapController
- *
- * @description :: Server-side logic for managing onemaps
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
+* OnemapController
+*
+* @description :: Server-side logic for managing onemaps
+* @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+*/
 
 module.exports = {
 	view: function (req, res) {
 		return res.view('onemap');
-		}
+	}
 };
